bc2dd3fb799d76464feb7c2013834516bdb55f59
Tony Bye <tbye@proofpoint.com>
2022-05-16 10:22:15 -0400

    better line reading

--- forEachLine.sh
+++ forEachLine.sh
@@ -1,7 +1,7 @@
 # First arg is the name of a file.
 # Second arg is a command, which is given one line as input
-while read line
+
+while IFS="" read -r line || [ -n "$line" ]
 do
   $2 $line
 done < $1
-
